a6da12962b7e1428311cf9148784cf8a8fa54db0
FireVulcain <n.disant@hotmail.com>
2021-02-11 14:24:30 +0100

    Updates

---
+++ precache-manifest.54ff76b240b10fb46cd389816d66f972.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a07ee948abb8c4675b75517602a97a",
+    "url": "/scannation-gh-pages/index.html"
+  },
+  {
+    "revision": "832a58f92cc074e963e6",
+    "url": "/scannation-gh-pages/static/css/main.548d2e53.chunk.css"
+  },
+  {
+    "revision": "cc278eefd4383757a77d",
+    "url": "/scannation-gh-pages/static/js/2.b41ddccb.chunk.js"
+  },
+  {
+    "revision": "186e19d2c71589ea2414b8dfb7b1fe42",
+    "url": "/scannation-gh-pages/static/js/2.b41ddccb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "832a58f92cc074e963e6",
+    "url": "/scannation-gh-pages/static/js/main.6b75eac6.chunk.js"
+  },
+  {
+    "revision": "ba2bfc5bb4ac1b10cb4d",
+    "url": "/scannation-gh-pages/static/js/runtime-main.d1f5da96.js"
+  },
+  {
+    "revision": "40f6187b88e47dee7c9ab68b47591954",
+    "url": "/scannation-gh-pages/static/media/fire.40f6187b.svg"
+  },
+  {
+    "revision": "b64d5d7235bf6d186c960cb40eccf5a0",
+    "url": "/scannation-gh-pages/static/media/logo.b64d5d72.png"
+  }
+]);
